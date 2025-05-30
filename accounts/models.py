8fb6440a64d24d3a6f935488530b0db09f548c30
rameshd07 <ramesh.2004dpm@gmail.com>
2025-05-30 15:03:53 +0530

    first commit

--- accounts/models.py
+++ accounts/models.py
@@ -119,6 +119,31 @@ class Vendor(models.Model):
         verbose_name_plural = 'Vendors'
 
 
+# class Customer(models.Model):
+#     first_name = models.CharField(max_length=256)
+#     last_name = models.CharField(max_length=256, blank=True, null=True)
+#     address = models.TextField(max_length=256, blank=True, null=True)
+#     email = models.EmailField(max_length=256, blank=True, null=True)
+#     phone = models.CharField(max_length=30, blank=True, null=True)
+#     loyalty_points = models.IntegerField(default=0)
+
+#     class Meta:
+#         db_table = 'Customers'
+
+#     def __str__(self) -> str:
+#         return self.first_name + " " + self.last_name
+
+#     def get_full_name(self):
+#         return self.first_name + " " + self.last_name
+
+#     def to_select2(self):
+#         item = {
+#             "label": self.get_full_name(),
+#             "value": self.id
+#         }
+#         return item
+
+
 class Customer(models.Model):
     first_name = models.CharField(max_length=256)
     last_name = models.CharField(max_length=256, blank=True, null=True)
@@ -131,14 +156,14 @@ class Customer(models.Model):
         db_table = 'Customers'
 
     def __str__(self) -> str:
-        return self.first_name + " " + self.last_name
+        return self.get_full_name()
 
     def get_full_name(self):
-        return self.first_name + " " + self.last_name
+        # Safely concatenate names even if last_name is None
+        return f"{self.first_name or ''} {self.last_name or ''}".strip()
 
     def to_select2(self):
-        item = {
+        return {
             "label": self.get_full_name(),
             "value": self.id
         }
-        return item
